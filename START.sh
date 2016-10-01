dddbaefa813ae714d4ba228c65b9e22375ed169d
Sándor Kazi <sandor.kazi@gmail.com>
2016-10-01 14:36:15 +0200

    partitioning done

--- START.sh
+++ START.sh
@@ -4,5 +4,13 @@
 # PLAYBOOK start for local desktop machine
 # ----------
 alias python=python2
+
+if [[ "${DISPLAY}" == "" ]]; then
+    echo "Please set DISPLAY for the visual settings!"
+    echo -n "    DISPLAY="
+    read DISPLAY
+    export DISPLAY
+fi
+
 ./library/prerequisites.sh
-ansible-playbook -K masu-desktop.yml -i localhost -vvv
+ansible-playbook -K masu-desktop.yml -i localhost
--- library/gsettings.py
+++ library/gsettings.py
@@ -60,19 +60,16 @@ class AnsibleGSettingModule(object):
                 pass
 
     def __init_dbus(self, user):
-        # type: (str)
         """
         Initializes the message bus.
         :param user: username
         """
         self.user = user
         dbus_output = subprocess.check_output('''
-            export DISPLAY=:0
-            ENABLED=$(sudo xhost | grep -c "SI:localuser:{user}")
-            echo "ENABLED=$ENABLED"
-            sudo xhost +SI:localuser:{user}
             sudo -H -u {user} -s /usr/bin/dbus-launch
-            '''.format(user=user),
+            '''.format(
+                user=user
+            ),
             shell=True
         )
         for line in dbus_output.split('\n'):
@@ -80,32 +77,31 @@ class AnsibleGSettingModule(object):
                 self.dbus_address = line.split('=', 1)[1]
             elif line.startswith('DBUS_SESSION_BUS_PID='):
                 self.dbus_pid = line.split('=', 1)[1]
-            elif line.startswith('ENABLED='):
-                self.enabled = line.split('=', 1)[1]
 
     def __execute(self, user, command):
-        # type: (str, str) -> str
         """
         Executes a given command with the `user` used as username and returns its output.
         :param user: username
         :param command: command to execute
         :return: execution output
-        :rtype: str
         """
         if self.dbus_pid is None:
             self.__init_dbus(user)
         gset = subprocess.check_output('''
-            export DISPLAY=:0
             export DBUS_SESSION_BUS_ADDRESS={dbus_address}
             export DBUS_SESSION_BUS_PID={dbus_pid}
             sudo -H -u {user} -s /bin/bash -c \'{command}\'
-            '''.format(dbus_address=self.dbus_address, dbus_pid=self.dbus_pid, user=user, command=command),
+            '''.format(
+                dbus_address=self.dbus_address,
+                dbus_pid=self.dbus_pid,
+                user=user,
+                command=command,
+            ),
             shell=True
         ).strip()
         return gset
 
     def get_param(self, user, schema, key):
-        # type: (str, str, str) -> str
         """
         Gets the `user`'s gsetting value for the given `key` in the given `schema`.
         :param user: username
@@ -118,7 +114,6 @@ class AnsibleGSettingModule(object):
         return self.__execute(user, command)
 
     def set_param(self, user, schema, key, value):
-        # type: (str, str, str, str) -> str
         """
         Sets the `user`'s gsetting value to the `value` specified for the given `key` in the given `schema` and returns
         the new value.
@@ -135,7 +130,6 @@ class AnsibleGSettingModule(object):
         return value
 
     def reset_param(self, user, schema, key):
-        # type: (str, str, str) -> str
         """
         Resets the `user`'s gsetting value for the given `key` in the given `schema` and returns the new value.
         :param user: username
@@ -146,7 +140,6 @@ class AnsibleGSettingModule(object):
         self.__execute(user, command)
 
     def main(self, test=None):
-        # type: (dict) -> None
         """
         Executes the given module command.
         :type test: dictionary object for testing purposes only
