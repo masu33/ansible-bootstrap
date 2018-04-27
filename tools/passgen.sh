d7acbd067c97dbed43e4812d143d463b4625d5ae
Sandor Kazi <sandor.kazi@gmail.com>
2018-04-27 09:18:40 +0200

    slight changes

--- tools/passgen.sh
+++ tools/passgen.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# For generating passwords to be stored as a vault variable
 
 PASS=`python -c "from passlib.hash import sha512_crypt; import getpass; print sha512_crypt.using(rounds=5000).hash(getpass.getpass())"`
 
