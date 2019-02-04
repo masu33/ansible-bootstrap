49c04f89139b74ce17c4030ee12f05e5b21048cd
Sandor Kazi <sandor.kazi@gmail.com>
2019-02-04 23:10:10 +0100

    jmespath added to prerequisites

--- tools/prerequisites.sh
+++ tools/prerequisites.sh
@@ -8,14 +8,14 @@ DISTRO=$(
 if [ "${DISTRO}" == "ubuntu" ]; then
     sudo apt update
     sudo apt install -y python3 python3-pip
-    sudo pip3 install ansible
+    sudo pip3 install ansible jmespath
 elif [ "${DISTRO}" == "manjaro" ]; then
     yes | sudo pacman -S python3
-    sudo pip3 install ansible
+    sudo pip3 install ansible jmespath
 elif [ "${DISTRO}" == "\"ol\"" ]; then
     sudo yum-config-manager --enable ol*_developer_EPEL
     sudo yum install -y python-pip
-    sudo pip install ansible
+    sudo pip install ansible jmespath
 else
     echo "Unknown DISTRO: ${DISTRO}"
     exit 1
